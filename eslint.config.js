9d4003ecc46a28207bd83fc6412a4c771c79bb7f
yosri azzabou <Yosriazzaboup9@gmail.com>
2025-02-14 14:52:38 +0100

    fixes

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,19 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: '@typescript-eslint/parser', // Use the TypeScript parser
+      parserOptions: {
+        project: ['./tsconfig.json'], // Path to your tsconfig
+      },
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
+    plugins: ['react', 'react-hooks', '@typescript-eslint'], // Add @typescript-eslint plugin
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      '@typescript-eslint/explicit-module-boundary-types': 'warn', // Example TypeScript rule
+      'no-console': 'warn',
+      'react/prop-types': 'off',
     },
   },
-)
+]
